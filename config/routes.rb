d4d846504fd88619ee1e9e7877aa0b1c08989c66
hirekakusi6000 <scorpion4773@icloud.com>
2021-02-21 16:07:28 +0900

    linksコントローラーとviewファイルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'links/index'
+  get 'links/show'
+  get 'links/new'
+  get 'links/create'
+  get 'links/edit'
+  get 'links/update'
+  get 'links/destroy'
   devise_for :users, controllers: { 
     sessions: 'users/sessions',
     registrations: 'users/registrations',
