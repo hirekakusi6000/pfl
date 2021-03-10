23c91b78378e8224900deb51d391c34a60a9dd74
hirekakusi6000 <scorpion4773@icloud.com>
2021-03-10 16:42:18 +0900

    リンクボタンのデザイン設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_031658) do
+ActiveRecord::Schema.define(version: 2021_03_10_060246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,8 @@ ActiveRecord::Schema.define(version: 2021_03_10_031658) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "background_color"
+    t.string "font_color"
     t.index ["user_id"], name: "index_links_on_user_id"
   end
 
