db7d7898f73efc99ffc303e23e2742f4f87ecaa8
hirekakusi6000 <scorpion4773@icloud.com>
2021-03-10 17:02:47 +0900

    usersカラム削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_060246) do
+ActiveRecord::Schema.define(version: 2021_03_10_075951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,8 +112,6 @@ ActiveRecord::Schema.define(version: 2021_03_10_060246) do
     t.string "background_color"
     t.string "font_color"
     t.string "font_family"
-    t.string "link_background_color"
-    t.string "link_font_color"
     t.integer "open_status", default: 1, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
