5c6d9f6933f58cfe3e3b2a465975e076d93b9dec
hirekakusi6000 <scorpion4773@icloud.com>
2021-03-14 11:22:39 +0900

    サブプロフィール一覧に編集・削除ボタンを追加

--- app/controllers/subprofiles_controller.rb
+++ app/controllers/subprofiles_controller.rb
@@ -1,11 +1,13 @@
 class SubprofilesController < ApplicationController
   before_action :set_designs, only: %i[update create edit]
+
   def new
    @subprofile = Subprofile.new 
   end
 
   def create
     @subprofile = Subprofile.new(subprofile_params)
+    @subprofile.update(open_status_params)
     @subprofile.user_id = current_user.id
 
     if @subprofile.save
@@ -32,20 +34,26 @@ class SubprofilesController < ApplicationController
       @subprofile.update(subprofile_params)
       update = 0
      elsif params[:edit_background_color]
-      @subprofile.update(edit_background_color)
-      update = 0
-     elsif params[:edit_background_color]
-      @subprofile.update(edit_background_color)
+      @subprofile.update(background_color)
       update = 0
      elsif params[:edit_font_color]
-      @subprofile.update(edit_font_color)
+      @subprofile.update(font_color)
       update = 0
      elsif params[:edit_font_family]
-      @subprofile.update(edit_font_family)
+      @subprofile.update(font_family)
       update = 0
      elsif params[:open_status]
       @subprofile.update(open_status_params)
-      update = 0   
+      update = 0
+     elsif params[:edit_comment]
+      @subprofile.update(comment_params)
+      update = 0 
+     elsif params[:linK_background_color]
+      @subprofile.update(linK_background_color_params)
+      update = 0 
+     elsif params[:linK_font_color]
+      @subprofile.update(linK_font_color_params)
+      update = 0 
      end
 
     if update = 0
@@ -63,25 +71,32 @@ class SubprofilesController < ApplicationController
     @subprofiles = @q.result.order("created_at DESC").page(params[:page]).per(PER_PAGE)
   end
 
+  def destroy
+    subprofile = Subprofile.find(params[:id])
+    subprofile.destroy
+    redirect_to("/subprofiles")
+  end
+
+
   private
 
   def subprofile_params
     params.require(:subprofile).permit(:title,:pfl_text)
   end
 
- def edit_background_color
+ def background_color
   params.require(:subprofile).permit(
       :background_color
   )
  end
 
-def edit_font_color
+def font_color
   params.require(:subprofile).permit(
     :font_color
   )
  end
 
- def edit_font_family
+ def font_family
   params.require(:subprofile).permit(
     :font_family
   )
@@ -90,6 +105,24 @@ def edit_font_color
  def open_status_params
     params.require(:subprofile).permit(
     :open_status
+  )
+ end
+
+  def comment_params
+    params.require(:subprofile).permit(
+    :comment
+  )
+ end
+
+ def  linK_background_color_params
+    params.require(:subprofile).permit(
+    :link_background_color
+  )
+ end
+
+ def  linK_font_color_params
+    params.require(:subprofile).permit(
+    :link_font_color
   )
  end
 
