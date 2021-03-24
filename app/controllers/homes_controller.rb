244651912713278eee22a857fbe5c1c6ed951663
hirekakusi6000 <scorpion4773@icloud.com>
2021-03-25 01:25:45 +0900

    homesコントローラー復元

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,19 @@
+class HomesController < ApplicationController# 1ページの表示数
+  PER_PAGE = 50
+  def top
+    @q = User.ransack(params[:q])
+    @users = @q.result.order("created_at DESC").page(params[:page]).per(PER_PAGE)
+    if user_signed_in?
+      redirect_to("/users/#{current_user.id}")
+    end
+  end
+  
+  def about
+  end
+
+  def service
+  end
+
+  def privacy
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_20_125530) do
+ActiveRecord::Schema.define(version: 2021_03_23_143028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,19 @@ ActiveRecord::Schema.define(version: 2021_03_20_125530) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "websites", force: :cascade do |t|
+    t.string "type", null: false
+    t.string "account", null: false
+    t.string "comment"
+    t.string "insidecolor"
+    t.string "outsidecolor"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_websites_on_user_id"
+  end
+
   add_foreign_key "links", "users"
   add_foreign_key "subprofiles", "users"
+  add_foreign_key "websites", "users"
 end
