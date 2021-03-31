76a4819e990140573cd7a4c001c80464ca2761fb
hirekakusi6000 <scorpion4773@icloud.com>
2021-03-31 23:41:03 +0900

    twitterの名前とemalを変更したら、自動的に変更されるように設定

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -35,8 +35,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     provider = provider.to_s
  
     @user = User.find_for_oauth(request.env['omniauth.auth'])
+    data = request.env['omniauth.auth']
  
     if @user.persisted?
+      #ログイン時の処理
+      @user.name = data[:info][:name]
+      @user.email = data[:info][:email]
+      @user.save
       flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
       sign_in_and_redirect @user, event: :authentication
     else
